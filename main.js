af0b12327025cf2c582a0ef100f0ae508769994d
Stephanie Cushing <stephaniecushing@Stephanies-MacBook-Air.local>
2021-09-13 15:06:28 -0400

    More Junk for Vinny

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ let button = document.querySelector("button");
 button.addEventListener("click", function (){
     if 
 });
+
+//https://assets.change.org/photos/4/iy/mo/jXiYmoKXZbBxwwb-800x450-noPad.jpg?1593702885
